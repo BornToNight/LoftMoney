cb7a28b2006b6049019e9f1a875b673fa3339395
BornToNight <isako-vadim@yandex.ru>
2020-07-21 22:32:41 +0500

    ДЗ-2-1

--- app/src/main/java/ru/borntonight/loftmoney/MainActivity.java
+++ app/src/main/java/ru/borntonight/loftmoney/MainActivity.java
@@ -1,14 +1,52 @@
 package ru.borntonight.loftmoney;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import ru.borntonight.loftmoney.item.Item;
+import ru.borntonight.loftmoney.item.ItemAdapter;
+import ru.borntonight.loftmoney.item.ItemAdapterClick;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    ItemAdapter itemAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.priceRecycleView);
+        itemAdapter = new ItemAdapter();
+
+        recyclerView.setAdapter(itemAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false));
+
+        itemAdapter.addData(generateExpenses());
+        itemAdapter.addData(generateIncomes());
+        itemAdapter.setItemAdapterClick(new ItemAdapterClick() {
+            @Override
+            public void onItemClick(Item item) {
+                // todo действие при нажатии
+            }
+        });
+    }
+
+    private List<Item> generateExpenses() {
+        List<Item> items = new ArrayList<>();
+        items.add(new Item("Молоко", "50 ₽", R.color.colorExpense));
+        return items;
+    }
+
+    private List<Item> generateIncomes() {
+        List<Item> items = new ArrayList<>();
+        items.add(new Item("Премия", "7500 ₽", R.color.colorAppleGreen));
+        return items;
     }
 }
---
+++ app/src/main/java/ru/borntonight/loftmoney/item/Item.java
@@ -0,0 +1,27 @@
+package ru.borntonight.loftmoney.item;
+
+public class Item {
+
+    private String name;
+    private String price;
+    private Integer color;
+
+    public Item(String name, String price, Integer color) {
+        this.name = name;
+        this.price = price;
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public Integer getColor() {
+        return color;
+    }
+
+}
